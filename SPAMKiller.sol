cf1474c55ba4e528b094b7e6eeb5b123ea140384
Javad <90986306+robocups@users.noreply.github.com>
2025-12-10 06:07:21 +0000

    Update SPAMKiller.sol
    non evm test

--- SPAMKiller.sol
+++ SPAMKiller.sol
@@ -36,3 +36,38 @@ contract ShibaKiller is ERC20, Ownable {
         pair = _pair;
     }
 }
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract CryptoPunksReborn is ERC721, Ownable {
+    using Strings for uint256;
+
+    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant MINT_PRICE = 0.05 ether;
+    uint256 public totalSupply;
+    string public baseURI = "ipfs://QmYourBaseURIHere/";
+
+    constructor() ERC721("CryptoPunksReborn", "CPR") {}
+
+    function mint(uint256 quantity) external payable {
+        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
+        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
+
+        for (uint256 i = 0; i < quantity; i++) {
+            _safeMint(msg.sender, totalSupply++);
+        }
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
