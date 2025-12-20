8a0b1b619db2ab26036d60e47988f3f94afea56c
Javad <90986306+robocups@users.noreply.github.com>
2025-12-20 20:59:52 +0000

    Remove custom tax logic from _transfer function
    Removed custom transfer logic for tax calculation.

--- GovernanceToken.sol
+++ GovernanceToken.sol
@@ -75,8 +75,4 @@ function _transfer(address from, address to, uint256 amount) internal override {
         }
 
     }
-function _transfer(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
-            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
-            uint256 lpTax = tax - marketingTax;
+
