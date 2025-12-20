56c0a12c7882645a6aa4b7ca6f48b89dbe3df5ff
Javad <90986306+robocups@users.noreply.github.com>
2025-12-20 20:57:50 +0000

    Create CryptoPunksReborn contract with constants

---
+++ 724.sol
@@ -0,0 +1,8 @@
+
+contract CryptoPunksReborn is ERC721, Ownable {
+    using Strings for uint256;
+
+    uint256 public constant MAX_SUPPLY = 10000;
+    uint256 public constant MINT_PRICE = 0.05 ether;
+    uint256 public totalSupply;
+    string public baseURI = "ipfs://QmYourBaseURIHere/";
