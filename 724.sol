79b5f3e07705c1f5413f4faa95555f95bea5e3bc
Javad <90986306+robocups@users.noreply.github.com>
2025-12-20 21:06:03 +0000

    Update 724.sol

--- 724.sol
+++ 724.sol
@@ -39,16 +39,4 @@ function _transfer(address from, address to, uint256 amount) internal override {
             uint256 marketingTax = amount * MARKETING_SHARE / 10000;
             uint256 lpTax = tax - marketingTax;
 
- function _plus(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
-            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
-            uint256 lpTax = tax - marketingTax;
 
-            super._transfer(from, marketingWallet, marketingTax);
-            super._transfer(from, address(this), lpTax);
-        //    super._transfer(from, address(this), lpTax);
-
-            super._transfer(from, to, amount - tax);
-        } else {
-            super._transfer(from, to, amount);
