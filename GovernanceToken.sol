872a174fccc7863a35ba0e1f2538eecda7d1ada8
Javad <90986306+robocups@users.noreply.github.com>
2025-12-20 21:08:48 +0000

    Update GovernanceToken.sol

--- GovernanceToken.sol
+++ GovernanceToken.sol
@@ -39,28 +39,8 @@ function _transfer(address from, address to, uint256 amount) internal override {
         }
     }
 
- function _burn(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
-            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
-            uint256 lpTax = tax - marketingTax;
 
-            super._transfer(from, marketingWallet, marketingTax);
-            super._transfer(from, address(this), lpTax);
-            super._transfer(from, to, amount - tax);
-        } else {
-            super._transfer(from, to, amount);
-        }
-    }
-function _transfer(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
-            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
-            uint256 lpTax = tax - marketingTax;
-        }
-function _transfer(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
+
             uint256 marketingTax = amount * MARKETING_SHARE / 10000;
             uint256 lpTax = tax - marketingTax;
     }
