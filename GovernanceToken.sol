9238b041bcbf7f50a4bd718f9c5a40e5c2241bb5
Javad <90986306+robocups@users.noreply.github.com>
2025-12-20 20:46:11 +0000

    Update GovernanceToken.sol
    v25 fixed

--- GovernanceToken.sol
+++ GovernanceToken.sol
@@ -1,6 +1,11 @@
 // GovernanceToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+function _transfer(address from, address to, uint256 amount) internal override {
+        if (from == pair || to == pair) {
+            uint256 tax = amount * TAX_RATE / 10000;
+            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
+            uint256 lpTax = tax - marketingTax;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
