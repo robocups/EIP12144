e86150852a4c28345e7baea14ec954cd92893d24
robocups <robocups@live.com>
2025-12-23 17:29:12 +0000

    VER1.355

--- CryptoPunksReborn.sol
+++ CryptoPunksReborn.sol
@@ -1,205 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-   function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
+pragma solidity 0.8.20;
 
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
-    }
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
+contract SimpleToken is ERC20, Ownable {
+    uint256 public mintPrice = 0.001 ether;
+    uint256 public maxSupply = 1000000 * 10**decimals();
+    bool public isMintingOpen = true;
 
+    constructor() ERC20("MyToken", "MTK") Ownable(msg.sender) {}
 
-contract CryptoPunksReborn is ERC721, Ownable {
-    using Strings for uint256;
+    function mint() external payable {
+        require(isMintingOpen, "Minting is closed");
+        require(msg.value >= mintPrice, "Insufficient ETH");
+        require(totalSupply() + (10**decimals()) <= maxSupply, "Max supply reached");
 
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MINT_PRICE = 0.05 ether;
-    uint256 public totalSupply;
-    string public baseURI = "ipfs://QmYourBaseURIHere/";
-
-
-
-
-
-
-
-
-
-
-
-
-    constructor() ERC721("CryptoPunksReborn", "CPR") {}
-
- 
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
+        _mint(msg.sender, 10**decimals());
     }
 
     function withdraw() external onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
-}
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
-contract CryptoPunksReborn is ERC721, Ownable {
-    using Strings for uint256;
-
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MINT_PRICE = 0.05 ether;
-    uint256 public totalSupply;
-    string public baseURI = "ipfs://QmYourBaseURIHere/";
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-function _transfer(address from, address to, uint256 amount) internal override {
-        if (from == pair || to == pair) {
-            uint256 tax = amount * TAX_RATE / 10000;
-            uint256 marketingTax = amount * MARKETING_SHARE / 10000;
-            uint256 lpTax = tax - marketingTax;
-
-
-contract CryptoPunksReborn is ERC721, Ownable {
-    using Strings for uint256;
-
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MINT_PRICE = 0.05 ether;
-    uint256 public totalSupply;
-    string public baseURI = "ipfs://QmYourBaseURIHere/";
-
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
-contract CryptoPunksReborn is ERC721, Ownable {
-    using Strings for uint256;
-
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MINT_PRICE = 0.05 ether;
-    uint256 public totalSupply;
-    string public baseURI = "ipfs://QmYourBaseURIHere/";
-
-    constructor() ERC721("CryptoPunksReborn", "CPR") {}
-
-    function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
-
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
+    function setMintPrice(uint256 _price) external onlyOwner {
+        mintPrice = _price;
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
-    }
-
-    function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
-    }
-}
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-
-contract CryptoPunksReborn is ERC721, Ownable {
-    using Strings for uint256;
-
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 public constant MINT_PRICE = 0.05 ether;
-    uint256 public totalSupply;
-    string public baseURI = "ipfs://QmYourBaseURIHere/";
-
-    constructor() ERC721("CryptoPunksReborn", "CPR") {}
-
-    function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
-
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
-    }
-
-    function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
-    }
-}
-    function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
-
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
-    }
-
-    function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
-    }
-}
-
-    function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
-
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
-    }
-
-    function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
-    }
-}
-    function mint(uint256 quantity) external payable {
-        require(totalSupply + quantity <= MAX_SUPPLY, "Sold out");
-        require(msg.value >= MINT_PRICE * quantity, "Insufficient ETH");
-
-        for (uint256 i = 0; i < quantity; i++) {
-            _safeMint(msg.sender, totalSupply++);
-        }
-    }
-
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        return string(abi.encodePacked(baseURI, tokenId.toString(), ".json"));
-    }
-
-    function withdraw() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
+    function toggleMinting() external onlyOwner {
+        isMintingOpen = !isMintingOpen;
     }
 }
